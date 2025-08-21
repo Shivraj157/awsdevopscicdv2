d7f14de3629325d3991aa89a26dd2bccc120f3c1
Shivraj Kashinath Jadhav <shivraj.jadhav@Yash.com>
2025-08-21 12:43:18 +0530

    see result

--- scripts/install_nginx.sh
+++ scripts/install_nginx.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+set -e
+echo "Installing Nginx..."
 sudo apt-get update -y
 sudo apt-get install -y nginx
--- scripts/start_nginx.sh
+++ scripts/start_nginx.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+set -e
+echo "Starting Nginx..."
 sudo systemctl enable nginx
 sudo systemctl restart nginx
